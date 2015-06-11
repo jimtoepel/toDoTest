5052a45322543bb0fae1d4259c1e89d8c2022843
jimtoepel <jim.toepel@gmail.com>
2015-06-10 19:58:08 -0700

    Initial Commit

---
+++ toDoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  toDoTest
+//
+//  Created by Jim Toepel on 6/10/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ toDoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  toDoTest
+//
+//  Created by Jim Toepel on 6/10/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
